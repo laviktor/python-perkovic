bc7a6fd9a01aa831bd9802612e852039be0ffd8b
V. Kato La <56576057+laviktor@users.noreply.github.com>
2022-04-06 21:43:52 -0400

    Update chapter_2_exercises.py

--- chapter_2_exercises.py
+++ chapter_2_exercises.py
@@ -1,7 +1,7 @@
-#2.12
+# 2.12
 4356 // 61
 4365 % 61
-#2.13
+# 2.13
 s1 = '-'
 s2 = '+'
 s1+s2
@@ -9,9 +9,15 @@ s1+s2+s1
 (s2+2*s1)*2
 (s2+2*s1)*10+s2
 (s2+s1+3*s2+2*s1)*5
-#2.14
+# 2.14
 s14='abcdefghijklmnopqrstuvwxyz'
 s14[0], s14[2], s14[25], s14[24], s14[16]
-#2.15
+# 2.15
 s15 = 'goodbye'
-  
+s15[0]=='g'
+s15[6]=='g'
+# (c) the first two characters are 'g' and 'a'
+s15[-2]=='x'
+# (e) the middle character is 'd'
+s15[0]==s15[-1]
+s15[-4]+s15[-3]+s15[-2]+s15[-1]=='tion'

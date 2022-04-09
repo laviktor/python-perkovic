7336cfe389e21559bceb99b5210a21abede93c75
V. Kato La <56576057+laviktor@users.noreply.github.com>
2022-04-09 05:21:28 -0400

    Update chapter_3_practice_problems.py

--- chapter_3_practice_problems.py
+++ chapter_3_practice_problems.py
@@ -1,7 +1,18 @@
 # 3.1
 fah = eval(input('Enter the temperature in degrees Fahrenheit: '))
 cel = (5/9)*(fah - 32)
-cel_conversion = 'The temperature in degrees Celsius is '
-# The following code is incorrect. It tries to concatenate string and float
-# print(cel_conversion + cel)
- 
+cel_convert = 'The temperature in degrees Celsius is '
+# The following code is incorrect. It tries to concatenate string and float:
+# print(cel_convert + cel)
+print(cel_convert, cel)
+# 3.2
+# (a)
+age = eval(input('Enter your age: '))
+if age > 62:
+ print('You can get your pension benefits.')
+# (b)
+name = input('Enter a baseball player''s last name: ')
+list2 = ['Musial', 'Aaraon', 'Williams', 'Gehrig', 'Ruth']
+if name in list2:
+ print('One of thetop 5 baseball players, ever!')
+# (c)

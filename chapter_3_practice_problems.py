bb1bd3150b38625cf21e7063bcaf5dd74ca78451
V. Kato La <56576057+laviktor@users.noreply.github.com>
2022-04-09 02:55:00 -0400

    Update chapter_3_practice_problems.py

--- chapter_3_practice_problems.py
+++ chapter_3_practice_problems.py
@@ -4,3 +4,4 @@ cel = (5/9)*(fah - 32)
 cel_conversion = 'The temperature in degrees Celsius is '
 # The following code is incorrect. It tries to concatenate string and float
 # print(cel_conversion + cel)
+ 

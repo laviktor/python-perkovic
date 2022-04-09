612aadce3a2451beb84f274ba701f1e62c04df2a
V. Kato La <56576057+laviktor@users.noreply.github.com>
2022-04-09 02:15:13 -0400

    Update chapter_3_practice_problems.py

--- chapter_3_practice_problems.py
+++ chapter_3_practice_problems.py
@@ -2,4 +2,5 @@
 fah = eval(input('Enter the temperature in degrees Fahrenheit: '))
 cel = (5/9)*(fah - 32)
 cel_conversion = 'The temperature in degrees Celsius is '
-print(cel_conversion + cel)
+# The following code is incorrect. It tries to concatenate string and float
+# print(cel_conversion + cel)

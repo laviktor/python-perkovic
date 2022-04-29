648879ef54223fd85f7376352b2db2117998b914
Kato La <56576057+laviktor@users.noreply.github.com>
2022-04-29 00:00:00 -0400

    Update chapter_3_exercises.py

--- chapter_3_exercises.py
+++ chapter_3_exercises.py
@@ -46,8 +46,8 @@ for i in range(0, 4, 3):
   print(i, end=" ")
 for i in range(5, 22, 4):
   print(i, end=" ")
-# 3.23
-list23 = eval(input("Enter list of words: "))
-for i in list23:
-  if 'secret' != list23[i]:
+# 3.22
+list22 = eval(input("Enter list of words: "))
+for i in list22:
+  if 'secret' != list22[i]:
     print(i)
